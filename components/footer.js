f003a9bf4ee989116a5daa0b3986876bd9d5bb2f
Anne-Lise Coruble <acoruble@yahoo.com>
2020-09-21 20:32:23 +0200

    Update footer.js

--- components/footer.js
+++ components/footer.js
@@ -7,7 +7,7 @@ export default function Footer() {
       <Container>
         <div className="py-28 flex flex-col lg:flex-row items-center">
           <h3 className="text-4xl lg:text-5xl font-bold tracking-tighter leading-tight text-center lg:text-left mb-10 lg:mb-0 lg:pr-4 lg:w-full">
-            Statically Generated with Next.js.
+            Généré statiquement avec Next.js.
           </h3>
         </div>
       </Container>
