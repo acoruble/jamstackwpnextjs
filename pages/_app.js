9deb1500101cf6587ab67ef23daefa0b92906260
Anne-Lise Coruble <acoruble@yahoo.com>
2020-09-23 18:35:34 +0200

    change text size

--- pages/_app.js
+++ pages/_app.js
@@ -4,15 +4,6 @@ function MyApp({ Component, pageProps }) {
   return (
     <div>
       <Component {...pageProps} />
-      <style jsx global>{`
-                  @font-face {
-                    font-family: 'MontserratAlternates';
-                    src: url('../public/fonts/MontserratAlternates-Regular.ttf');
-                    font-weight: bold;
-                    font-style: normal;
-                    font-display: swap;
-                  }
-                `}</style>
     </div>
   );
 }
